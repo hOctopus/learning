6d93f3f66e46ad052d4ea8e6cd7ac01b147f1b9d
Helena Suess <helenasuess@gmail.com>
2015-11-23 17:19:16 -0600

    added some comments

--- simple_socket/lib/browser.rb
+++ simple_socket/lib/browser.rb
@@ -1,5 +1,12 @@
+# Browser for the simple socket projects on The Odin Project
+# Credit goes to the example Ruby server linked from The Odin Project, which helped me with the regular expressions...
+# ...and to other students, whose solutions were invaluable in helping me understand how this project works...
+# ...and whose code I borrowed and modified to that purpose.
+
 require 'socket'
 require 'json'
+
+# WORKING DIRECTORY MUST MATCH PROGRAM DIRECTORY OR THIS WILL NOT WORK
 
 host = "localhost"
 port = 3000
@@ -10,8 +17,10 @@ puts 'for POST requests, use format "POST [path] [name] [email]"'
 puts 'don\'t worry about case-sensitivity, i\'ll handle that.'
 puts "enter \"QUIT\" to exit the browser\n\n"
 
-def parse(input)
-  params = Hash.new { |hash, key| hash[key] = Hash.new }
+# this method figures out the appropriate request to send to the server...
+# ...based on the user's input, allowing the user to type a single line at the prompt
+def parse(input) # input comes as an array for easier parsing
+  params = Hash.new { |hash, key| hash[key] = Hash.new } # hash for content to be sent in POST request
   command = input[0].upcase
   path = input[1]
   case command
@@ -23,7 +32,7 @@ def parse(input)
     when "POST"
       params[:person][:name] = input[2..-2].join(" ")
       params[:person][:email] = input[-1]
-      body = params.to_json
+      body = params.to_json     # conversion to JSON so server can parse and convert to HTML
       return "POST #{path} HTTP/1.0\r\nTime is: #{Time.now.ctime}\r\nContent-type: text/html\r\nServer: Simple Socket Server\r\nSize of Content: #{body.length} characters\r\n\r\n#{body}"
     else
       puts "I didn't understand. Please use command GET, POST, or QUIT."
@@ -31,19 +40,22 @@ def parse(input)
     end
 end
 
-loop do
+# User input is given in a single line at the command prompt
+# The input is run through the parse method, and the appropriate request is sent to the server
+# Then the input is printed in HTML format, with a header showing content type, size, and date/time
+loop do # browser runs until user enters quit command
   print 'enter request: '
   input = gets.chomp
-  request = parse(input.split(" "))
-  unless request
+  request = parse(input.split(" "))   # find the appropriate request to send
+  unless request                      # try again if input wasn't valid
     redo
   else
     socket = TCPSocket.open(host, port)
     socket.print(request)
     response = socket.read
-    header, body = response.split("\r\n\r\n", 2)
+    header, body = response.split("\r\n\r\n", 2) # separates the response header from the printed output
     puts ''
-    print body
+    print body # printed output includes its own header for the user to read
     socket.close
   end
 end
--- simple_socket/lib/server.rb
+++ simple_socket/lib/server.rb
@@ -1,12 +1,19 @@
+# Server for the simple socket projects on The Odin Project
+# Credit goes to the example Ruby server linked from The Odin Project, which helped me with the regular expressions...
+# ...and to other students, whose solutions were invaluable in helping me understand how this project works...
+# ...and whose code I borrowed and modified to that purpose.
+
 require 'socket'
 require 'json'
 
+# WORKING DIRECTORY MUST MATCH PROGRAM DIRECTORY OR THIS WILL NOT WORK
+
 server = TCPServer.open(3000)
-loop do
-  Thread.start(server.accept) { |client|
-    request = client.read_nonblock(256)
+loop do # server runs forever
+  Thread.start(server.accept) { |client| # allows for mutiple simultaneous requests, if that ever happens
+    request = client.read_nonblock(256)  # makes sure server doesn't wait forever for the end of input
     method = request.split(" ")[0]
-    path = Dir.pwd + request.gsub(/(GET |POST )/,'').gsub(/ HTTP.*/,'').split("\s")[0]
+    path = Dir.pwd + request.gsub(/(GET |POST )/,'').gsub(/ HTTP.*/,'').split("\s")[0] # finds HTML in CWD
     header, body = request.gsub(/.*(?=HTTP)/,'').split("\r\n\r\n")
     unless File.exist?(path)
       client.puts "HTTP/1.1 404 Not Found\r\n\r\n"
